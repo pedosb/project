6762da41cc580e3f096a541696ff568cd066d8d1
User 1 <user1@ufpa.br>
2016-07-08 17:29:10 -0300

    Add tree numbers

--- main.c
+++ main.c
@@ -3,13 +3,13 @@ int mul(int i, int j) {
 	return (i * j);
 }
 
-int add(int i, int j) {
-	return(i + j);
+int add(int i, int j, int k) {
+	return(i + j + k);
 }
 
 int main(int argc, char **argv) 
 {
 	printf("Mulply 3 and 2 equals %d\n", mul(3, 2));
 	printf("Hello %s\n", argv[1]);
-	printf("Add 3 + 2 equals %d\n", add(3, 2));
+	printf("Add 3 + 2 + 8 equals %d\n", add(3, 2, 8));
 }

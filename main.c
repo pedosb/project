2d30c8a779a1c6e477df85951d3749de7d82435e
User 1 <user1@ufpa.br>
2016-07-08 17:21:59 -0300

    Add the add function

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int add(int i, int j) {
+	return(i + j);
+}
+
 int main(int argc, char **argv) 
 {
 	printf("Hello %s\n", argv[1]);
+	printf("Add 3 + 2 equals %d\n", add(3, 2));
 }

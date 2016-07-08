b04dc270995998835ff94566a0d1f536e127cb17
User 2 <user2@ufpa.br>
2016-07-08 17:24:20 -0300

    Add the mul function

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+int mul(int i, int j) {
+	return (i * j);
+}
 
 int main(int argc, char **argv) 
 {
+	printf("Mulply 3 and 2 equals %d\n", mul(3, 2));
 	printf("Hello %s\n", argv[1]);
 }
